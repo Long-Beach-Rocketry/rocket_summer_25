f5af5f625551c2704e71a057e3f196b10e3ad846
TJMalaska <malaskatj@gmail.com>
2025-06-02 22:53:12 -0700

    messing with timers I can log at 80 MHz but if i use the cli too much i die

--- app/flight_computer_subscale/BSP_L476/flight_computer_subscale_bsp.c
+++ app/flight_computer_subscale/BSP_L476/flight_computer_subscale_bsp.c
@@ -66,7 +66,7 @@ bool BSP_Init(Usart* usart, Spi* spi, I2c* i2c, Gpio* red_led, Gpio* green_led,
 
     // PB6 PB7 AF 4
     const StGpioSettings i2c_conf = {ALT_FUNC, OPEN_DRAIN, 0, PULL_UP, 0x4};
-    EXIT_IF_FAIL(GiveStI2c(i2c, &memory, time, I2C1_BASE, 0x20B,
+    EXIT_IF_FAIL(GiveStI2c(i2c, &memory, time, I2C1_BASE, 0xD09Be3,
                            (StGpioParams){{0}, GPIOB_BASE, 6, i2c_conf},
                            (StGpioParams){{0}, GPIOB_BASE, 7, i2c_conf}));
     __enable_irq();
@@ -96,29 +96,30 @@ void TIM1_UP_TIM16_IRQHandler(void)
     HAL_IncTick();
 }
 
+//new one/**
 void SystemClock_Config(void)
 {
     RCC_OscInitTypeDef RCC_OscInitStruct = {0};
     RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-    /** 
-     * Configure the main internal regulator output voltage
-     */
+    /** Configure the main internal regulator output voltage
+  */
     if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
     {
         Error_Handler();
     }
 
-    /** 
-     * Initializes the RCC Oscillators according to the specified parameters
-     * in the RCC_OscInitTypeDef structure.
-     */
-    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_MSI;
+    RCC_OscInitStruct.MSIState = RCC_MSI_ON;
+    RCC_OscInitStruct.MSICalibrationValue = 0;
+    RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_6;
     RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
     RCC_OscInitStruct.PLL.PLLM = 1;
-    RCC_OscInitStruct.PLL.PLLN = 8;
+    RCC_OscInitStruct.PLL.PLLN = 20;
     RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
     RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
     RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
@@ -127,9 +128,8 @@ void SystemClock_Config(void)
         Error_Handler();
     }
 
-    /** 
-     * Initializes the CPU, AHB and APB buses clocks
-     */
+    /** Initializes the CPU, AHB and APB buses clocks
+  */
     RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
                                   RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
     RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
@@ -137,11 +137,59 @@ void SystemClock_Config(void)
     RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
     RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
     {
         Error_Handler();
     }
 }
+
+// old one
+//void systemclock_config(void)
+//{
+//    rcc_oscinittypedef rcc_oscinitstruct = {0};
+//    rcc_clkinittypedef rcc_clkinitstruct = {0};
+//
+//    /**
+//     * configure the main internal regulator output voltage
+//     */
+//    if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
+//    {
+//        Error_Handler();
+//    }
+//
+//    /**
+//     * Initializes the RCC Oscillators according to the specified parameters
+//     * in the RCC_OscInitTypeDef structure.
+//     */
+//    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+//    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+//    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+//    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+//    RCC_OscInitStruct.PLL.PLLM = 1;
+//    RCC_OscInitStruct.PLL.PLLN = 8;
+//    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
+//    RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+//    RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+//    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+//    {
+//        Error_Handler();
+//    }
+//
+//    /**
+//     * Initializes the CPU, AHB and APB buses clocks
+//     */
+//    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+//                                  RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+//    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+//    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+//    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+//    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+//
+//    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_3) != HAL_OK)
+//    {
+//        Error_Handler();
+//    }
+//}
 
 void Error_Handler(void)
 {
