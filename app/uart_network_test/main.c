0f3a5f8bedf9875b0aa1599634699bcfcf4efb76
TJMalaska <malaskatj@gmail.com>
2025-06-09 17:40:39 -0700

    save

--- app/uart_network_test/main.c
+++ app/uart_network_test/main.c
@@ -5,10 +5,11 @@
 #include "uart_lbr_net.h"
 #include "uart_network_bsp.h"
 //#include "uart_network_test_app.h"
+
 #define ADDRESS 66
+#define RECEIVER_ADDRESS 41
 Usart usart;
 
-#define RECEIVER_ADDRESS 41
 uint8_t data = 1;
 size_t data_size = sizeof(data_size);
 network_if interface;
@@ -17,6 +18,7 @@ static void loop_func(void)
 {
     static size_t package_size;
     static uint8_t package_buffer[MAX_RECEIVE_BUF_SIZE];
+
     usart.send(&usart, (uint8_t*)"start\n", 6);
     interface.receive(package_buffer, &package_size);
     usart.send(&usart, (uint8_t*)"\n", 1);
